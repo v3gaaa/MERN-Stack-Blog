c906e3aa17a76ed8fa8788e1af5f4eab9cc9d29d
svsm03 <svsm03@hotmail.com>
2024-01-31 23:12:29 -0600

    Sign Up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,8 +10,7 @@ export default function Header() {
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
 
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 to-purple-900 rounded-lg text-white'>Vega's</span>
-            Blog
+            <span className='px-2 py-1 bg-gradient-to-r from-purple-500 to-purple-700 rounded-lg text-white'>Vega's</span>Blog
         </Link>
         <form action="">
             <TextInput 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,53 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-5'>
+
+        {/* left */}
+        <div className='flex-1'>
+          <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-purple-500 to-purple-700 rounded-lg text-white'>Vega's</span>Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a project to showcase my habilities with the MERN stack. Sign up to try it.
+          </p>
+        </div>
+          
+        {/* right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username'/>
+              <TextInput type='text' placeholder='Username' id='username'/>
+            </div>
+            <div>
+              <Label value='Your email'/>
+              <TextInput type='text' placeholder='Email' id='email'/>
+            </div>
+            <div>
+              <Label value='Your password'/>
+              <TextInput type='text' placeholder='Password' id='password'/>
+            </div>
+            <Button gradientMonochrome='purple' className='w-full' type='submit'>
+              Sign up
+            </Button>
+          </form>
+
+          <div className='flex gap-2 texr-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to="/sign-in" className='text-purple-500 font-semibold'>
+              Sign in
+            </Link>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
